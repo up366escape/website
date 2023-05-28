a751ed14e42b1eccd2ff273a4f91e3702a40973d
up366escape <134759197+up366escape@users.noreply.github.com>
2023-05-28 10:10:10 +0800

    Update changeUA.js
    Signed-off-by: up366escape <134759197+up366escape@users.noreply.github.com>

--- js/changeUA.js
+++ js/changeUA.js
@@ -1,5 +1,8 @@
-const UserAgent =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{version} Safari/537.36';
-Object.defineProperty(navigator,'userAgent',{
-  value:UserAgent,
-  writable:false
-});
+const PCUserAgent =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{version} Safari/537.36';
+const MBUserAgent =  'Mozilla/5.0 (Linux; Android 8.0.0; BZT-W09) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.28 Safari/537.36'
+function changeUA(ua){
+  Object.defineProperty(navigator,'userAgent',{
+    value:ua,
+    writable:false
+  });
+}
